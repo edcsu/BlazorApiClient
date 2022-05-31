0966b7bc0f879072ca6b03674bb907bbea08273d
Sewannonda Keith Edwin <skeith696@gmail.com>
2022-05-31 18:51:57 +0300

    refactor: fetched data component

---
+++ BlazorApiClient/Pages/FetchData.cs
@@ -0,0 +1,19 @@
+﻿using BlazorApiClient.Dtos;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorApiClient.Pages
+{
+    public partial class FetchData
+    {
+        [Inject]
+        private HttpClient? Http { get; set; }
+
+        private LaunchDto[]? launchDtos;
+
+        protected override async Task OnInitializedAsync()
+        {
+            launchDtos = await Http!.GetFromJsonAsync<LaunchDto[]>("https://api.spacex.land/rest/launches");
+        }
+    }
+}
